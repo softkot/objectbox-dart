24ea8d0a871dc92f1f1445913b7dca8f6215393c
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-12-08 16:53:55 +0100

    Dart native API initialization error handling

--- lib/src/sync.dart
+++ lib/src/sync.dart
@@ -495,7 +495,7 @@ class _SyncListenerGroup<StreamValueType> {
   }
 
   void _debugLog(String message) {
-    print('Listener ${name}: $message');
+    // print('Listener ${name}: $message');
   }
 }
 
